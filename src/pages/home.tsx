a57500b5469662b5027346ee8af3e7d7a28cd085
chirag7pandey <chirag7pandey@gmail.com>
2024-04-24 13:26:58 +0530

    initial commmit

---
+++ src/pages/home.tsx
@@ -0,0 +1,18 @@
+  import {Link} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      
+    <section>
+
+    </section>
+      <h1 >Latest Products
+      <Link to='/serch' className='findmore'>More</Link></h1>
+      
+
+    </div>
+  )
+}
+
+export default Home
